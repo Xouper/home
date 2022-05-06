a2d6eeac461346b0a55e4c2e66a127789328c441
Алексей Устинов <xouper@xouper.home>
2022-05-06 22:00:09 +0300

    stepik

--- 24solve/24-135.py
+++
@@ -1,6 +0,0 @@
-# Автор: В.Н. Шубинкин
-
-with open('24-j4.txt') as tf:
-    s = tf.read().strip()
-
-print(s.count('BOSS') - s.count('JBOSS') - s.count('BOSSJ') + s.count('JBOSSJ'))
--- main.py
+++
@@ -1,9 +0,0 @@
-
-# f = open("24data/24-157.txt")
-# file = f.read()
-# my_list = []
-# for i in range(1, len(file)-1):
-#     if file[i-1] == file[i+1]:
-#         my_list.append(file[i])
-# print(max(set(my_list), key=lambda x: my_list.count(x)), my_list[])
-# print(dict((x,my_list.count(x)) for x in set(my_list) if my_list.count(x) > 1))
